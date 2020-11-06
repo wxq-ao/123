e8324d56670594e9f0578c654ab6a408a1f18f19
wxq-ao <buaile@foxmail.com>
2020-11-06 23:01:12 +0800

    junit5第一次作业

---
+++ src/main/java/com/util/Calculator.java
@@ -0,0 +1,39 @@
+package com.util;
+
+public class Calculator {
+
+    public static int result = 0;
+
+    public static int add(int x , int y ){
+
+        return result = x + y ;
+
+    }
+
+    public static int count(int x  ){
+
+        result += x;
+
+        return result ;
+
+    }
+
+
+    public static int subtract(int x , int y){
+
+        return result = x - y ;
+
+    }
+
+    public static int multiply(int x , int y){
+
+        return result = x * y ;
+
+    }
+
+    public static int divide(int x , int y){
+
+        return result = x / y ;
+
+    }
+}
---
+++ src/test/java/com/junit5/demo/Junit5Demo.java
@@ -0,0 +1,65 @@
+package com.junit5.demo;
+
+import com.util.Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Junit5Demo {
+
+    @Test
+    public void addTest(){
+
+        int result = Calculator.add(1,2);
+        int result2 = Calculator.add(2,2);
+        assertAll("msg:多个组合断言assertAll",
+                ()-> assertEquals(3,result),
+                ()-> assertEquals(4,result2)
+
+
+        );
+    }
+
+    @Test
+    public void subtractTest(){
+
+        int result = Calculator.subtract(5,2);
+        System.out.println(result);
+        assertEquals(3,result);
+
+    }
+
+    @Test
+    public void multiplyTest(){
+
+        int result = Calculator.multiply(5,2);
+        System.out.println(result);
+        assertEquals(10,result);
+
+    }
+
+    @Test
+    public void divideest(){
+
+        int result = Calculator.divide(5,2);
+        System.out.println(result);
+        assertEquals(2,result);
+
+    }
+
+    @Test
+    public void countTest(){
+
+        Calculator.count(1);
+        Calculator.count(3);
+        Calculator.count(4);
+        int result = Calculator.count(1);
+        System.out.println(result);
+        assertEquals(9,result);
+
+    }
+
+
+
+}
